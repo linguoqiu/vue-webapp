67ceb832cd611b71118235043903609f1772f685
GAVIN <876575488@qq.com>
2017-09-28 11:28:56 +0800

    add  singer page

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>
-      歌手页面
+  <div class="singer">
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-    
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+
+export default {
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singers = res.data.list
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style scoped lang="scss">
+@import '../../common/stylus/variable.scss';
 
+.singer {
+  position: fixed;
+  top: 88px;
+  bottom: 0;
+  width: 100%;
+}
 </style>
