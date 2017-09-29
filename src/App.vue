413c3bf32a91b6cff6b18d2d987ed5892abbc3a4
GAVIN <876575488@qq.com>
2017-09-29 16:11:15 +0800

    添加切换页面时的效果

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <div id="app">
     <m-header></m-header>
     <tab></tab>
-    <keep-alive>
-      <router-view></router-view>
-    </keep-alive>
+    <transition name="anim" mode="out-in">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
   </div>
 </template>
 
@@ -22,4 +24,27 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.slide-left-enter,
+.slide-right-leave-active {
+  opacity: 0;
+  -webkit-transform: translate(50px, 0);
+  transform: translate(50px, 0);
+}
+
+.slide-left-leave-active,
+.slide-right-enter {
+  opacity: 0;
+  -webkit-transform: translate(-50px, 0);
+  transform: translate(-50px, 0);
+}
+
+.anim-enter-active,
+.anim-leave-active {
+  transition: opacity .5s ease;
+}
+
+.anim-enter,
+.anim-leave-active {
+  opacity: 0;
+}
 </style>
