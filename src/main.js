f0c77c132a59889aa876cf9d4870099a7b05eeef
gavin <876575488@qq.com>
2017-10-09 22:18:04 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -12,8 +12,11 @@ Vue.config.productionTip = false
 // fastclick添加到body上，这样可以消除 所有的非按钮的点击事件的300ms的延迟
 fastclick.attach(document.body)
 
+// 配置图片懒加载
 Vue.use(VueLazyLoad, {
-    loading: require('common/image/default.png')
+    // common/image/default.png 这是小鸡的图片
+    // 以下为配置没请求到图片的时候，默认展示的图片
+    loading: require('components/m-header/logo@2x.png')
 })
 
 /* eslint-disable no-new */
