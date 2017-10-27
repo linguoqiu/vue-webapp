658a6c688eb3e4a5fc6b4fb3bdbb12833cdf4f66
Gavin <876575488@qq.com>
2017-10-27 23:13:04 +0800

    完善 歌单页面

---
+++ src/base/song-list/song-list.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="song-list">
+      <ul>
+          <!-- 歌单列表 -->
+          <li v-for="song in songs" class="item">
+              <div class="content">
+                  <h2 class="name">{{song.name}}</h2>
+                  <p class="desc">{{getDesc(song)}}</p>
+              </div>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+    props: {
+        songs: {
+            type: Array,
+            default: []
+        }
+    },
+    methods: {
+        getDesc(song) {
+            return `${song.singer} * ${song.album}`
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~common/stylus/variable";
+@import "~common/stylus/mixin";
+
+.song-list{
+    .item{
+      display: flex;
+      align-items: center;
+      box-sizing: border-box;
+      height: 64px;
+      font-size: $font-size-medium;
+      .rank{
+        flex: 0 0 25px;
+        width: 25px;
+        margin-right: 30px;
+        text-align: center;
+        .icon{
+          display: inline-block;
+          width: 25px;
+          height: 24px;
+          background-size: 25px 24px;
+          &.icon0{
+            @include bg-image('first');
+          }
+          &.icon1{
+            @include bg-image('second');
+          }
+          &.icon2{
+            @include bg-image('third');
+            }
+        }
+        .text{
+          color: $color-theme;
+          font-size: $font-size-large;
+        }
+      }
+      .content{
+        flex: 1;
+        line-height: 20px;
+        overflow: hidden;
+        .name{
+          @include no-wrap();
+          color: $color-text;
+        }
+        .desc{
+            @include no-wrap();
+          margin-top: 4px;
+          color: $color-text-d;
+          }
+      }
+    }
+}
+</style>
+
+
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -4,14 +4,33 @@
           <i class="icon-back"></i>
       </div>
       <h1 class="title" v-html="title"></h1>
-      <div class="bg-image" :style="bgStyle">
-          <div class="filter"></div>
+      <div class="bg-image" :style="bgStyle" ref="bgImage">
+          <div class="filter" ref="filter"></div>
       </div>
+      <!-- 背景层，用于滚动 -->
+      <div class="bg-layer" ref="layer"></div>
+      <!-- 滚动组件 -->
+      <scroll @scroll="scroll" :probe-type="probeType" :listen-scroll="listenScroll" :data="songs" class="list" ref="list">
+          <div class="song-list-wrapper">
+              <!-- 歌单组件 -->
+             <song-list :songs="songs"></song-list>
+          </div>
+      </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import Scroll from 'base/scroll/scroll'
+import SongList from 'base/song-list/song-list'
+
+const RESERVED_HEIGHT = 40
+
 export default {
+    data() {
+        return {
+            scrollY: 0
+        }
+    },
     props: {
         bgImage: {
             type: String,
@@ -29,6 +48,63 @@ export default {
     computed: {
         bgStyle() {
             return `background-image:url(${this.bgImage})`
+        }
+    },
+    created() {
+        this.probeType = 3
+        this.listenScroll = true
+    },
+    components: {
+        Scroll,
+        SongList
+    },
+    mounted() {
+        // 缓存图片的高度
+        this.imageHeight = this.$refs.bgImage.clientHeight
+        // 最小滚动值
+        this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT
+        // list的高度等于bgImage的高度
+        this.$refs.list.$el.style.top = `${this.imageHeight}px`
+    },
+    watch: {
+        scrollY(newY) {
+            let translateY = Math.max(this.minTranslateY, newY)
+            let zIndex = 0
+            let scale = 1
+            let blur = 0 // 图片模糊参数
+            // 下拉歌单的时候，图片无缝对接放大的比列
+            const percent = Math.abs(newY / this.imageHeight)
+            // 为layer添加transform样式
+            this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
+            this.$refs.layer.style['webkitTransform'] = `translate3d(0,${translateY}px,0)`
+            if (newY > 0) {
+                scale = 1 + percent
+                zIndex = 10
+            } else {
+                blur = Math.min(20 * percent, 20)
+            }
+            // backdrop-filter 高斯模糊的属性，只有在ios上能看到效果
+            this.$refs.filter.style['backdrop-filter'] = `blur(${blur}px)`
+            this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur}px)`
+            // 处理上划的时候，歌曲在背景图上面没隐藏的情况
+            if (newY < this.minTranslateY) {
+                zIndex = 10
+                this.$refs.bgImage.style.paddingTop = 0
+                this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+            } else {
+                this.$refs.bgImage.style.paddingTop = '70%'
+                this.$refs.bgImage.style.height = 0
+            }
+            this.$refs.bgImage.style.zIndex = zIndex
+            // 为bgImage添加transform 放大样式
+            this.$refs.bgImage.style['transform'] = `scale(${scale})`
+            this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
+        }
+    },
+    methods: {
+        //  scroll组件发出来的scroll事件的相应方法
+        scroll(pos) {
+            this.scrollY = pos.y
         }
     }
 }
