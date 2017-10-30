94576e4c7a6fa33358528a666a5fdb6e4f6675df
Gavin <876575488@qq.com>
2017-10-30 22:21:24 +0800

    完善 播放页面

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,22 +1,158 @@
 <template>
   <div class="player" v-show="playList.length > 0">
-      <div class="normal-player" v-show="fullScreen">
-          播放器
-      </div>
-      <div class="mini-player" v-show="!fullScreen"></div>
+      <!-- 监听transition的钩子事件 -->
+      <transition name="normal" @enter="enter" @after-enter="afterEnter" @leave="leave" @after-leave="afterLeave">
+        <div class="normal-player" v-show="fullScreen">
+            <!-- 背景图 -->
+            <div class="background">
+                <img width="100%" height="100%" :src="currentSong.image">
+            </div>
+            <!-- 返回按钮和title -->
+            <div class="top">
+                <div class="back" @click='back'>
+                    <i class="icon-back"></i>
+                </div>
+                <h1 class="title" v-html="currentSong.name"></h1>
+                <h2 class="subtitle" v-html="currentSong.singer"></h2>
+            </div>
+            <!-- 唱片图片 -->
+            <div class="middle">
+                <div class="middle-l">
+                    <div class="cd-wrapper" ref="cdWrapper">
+                        <div class="cd">
+                            <img class="image" :src="currentSong.image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- 底部的操作区 -->
+            <div class="bottom">
+                <div class="operators">
+                    <div class="icon i-left">
+                        <i class="icon-sequence"></i>
+                    </div>
+                    <div class="icon i-left">
+                        <i class="icon-prev"></i>
+                    </div>
+                    <div class="icon i-center">
+                        <i class="icon-play"></i>
+                    </div>
+                    <div class="icon i-right">
+                        <i class="icon-next"></i>
+                    </div>
+                    <div class="icon i-right">
+                        <i class="icon icon-not-favorite"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </transition>
+      <!-- mini-player -->
+       <transition name="mini">
+            <div class="mini-player" v-show="!fullScreen" @click='open'>
+                <div class="icon">
+                    <img width="40" height="40" :src="currentSong.image">
+                </div>
+                <div class="text">
+                    <h2 class="name" v-html="currentSong.name"></h2>
+                    <p class="desc" v-html="currentSong.singer"></p>
+                </div>
+                <div class="control"></div>
+                <div class="control">
+                    <i class="icon-playlist"></i>
+                </div>
+            </div>
+       </transition>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import { mapGetters } from 'vuex'
+import { mapGetters, mapMutations } from 'vuex'
+import animations from 'create-keyframe-animation'
+import { prefixStyle } from 'common/js/dom'
+
+const transform = prefixStyle('transform')
 
 export default {
     computed: {
         // 获取vuex的状态
         ...mapGetters([
             'fullScreen',
-            'playList'
+            'playList',
+            'currentSong'
         ])
+    },
+    methods: {
+        // 关闭大播放器界面
+        back() {
+            this.setFullScreen(false)
+        },
+        // 开启大播放器界面
+        open() {
+            this.setFullScreen(true)
+        },
+        // done为执行函数，执行后会跳转到afterEnter
+        enter(el, done) {
+            const { x, y, scale } = this._getPosAndScale()
+            // 定义动画（定义动画帧）,使用到create-keyframe-animation插件
+            let animation = {
+                0: {
+                    transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+                },
+                60: {
+                    transform: `translate3d(0,0,0) scale(${1.1})`
+                },
+                100: {
+                    transform: `translate3d(0,0,0) scale(${1})`
+                }
+            }
+            // 注册动画
+            animations.registerAnimation({
+                name: 'move', // 动画名称
+                animation,
+                presets: { // 动画预设的内容
+                    duration: 400,
+                    easing: 'linear'
+                }
+            })
+            // 运行动画:第一个参数为动画的dom，第二个为指定的动画名称， 第三个为回调函数
+            animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+        },
+        afterEnter() {
+            // 清空animation
+            animations.unregisterAnimation('move')
+            this.$refs.cdWrapper.style.animation = ''
+        },
+        leave(el, done) {
+            this.$refs.cdWrapper.style.transition = `all 0.4s`
+            const { x, y, scale } = this._getPosAndScale()
+            this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`
+            // 监听transitionend事件，完成后执行done
+            this.$refs.cdWrapper.addEventListener('transitionend', done)
+        },
+        afterLeave() {
+             // 清空transition
+            this.$refs.cdWrapper.style.transition = ''
+             this.$refs.cdWrapper.style[transform] = ''
+        },
+        _getPosAndScale() {
+            const targetWidth = 40 // 小图宽度
+            const paddingLeft = 40  // 小图左边padding
+            const paddingBottom = 30 // 小图下边padding
+            const paddingTop = 80  // 大图上边的padding
+            const width = window.innerHeight * 0.8 // 大图的宽度
+            const scale = targetWidth / width // 缩放比例
+            const x = -(window.innerWidth / 2 - paddingLeft) // 偏移量x
+            const y = window.innerHeight - paddingTop - width / 2 - paddingBottom // 偏移量y
+            return {
+                x,
+                y,
+                scale
+            }
+        },
+        ...mapMutations({
+            setFullScreen: 'SET_FULL_SCREEN'
+        })
     }
 }
 </script>
@@ -25,296 +161,303 @@ export default {
 @import "~common/stylus/variable";
 @import "~common/stylus/mixin";
 
-.player{
-    .normal-player{
-      position: fixed;
+.player {
+  .normal-player {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    z-index: 150;
+    background: $color-background;
+    .background {
+      position: absolute;
       left: 0;
-      right: 0;
       top: 0;
-      bottom: 0;
-      z-index: 150;
-      background: $color-background;
-      .background{
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+      opacity: 0.6;
+      filter: blur(20px);
+    }
+    .top {
+      position: relative;
+      margin-bottom: 25px;
+      .back {
+        position: absolute;
+        top: 0;
+        left: 6px;
+        z-index: 50;
+        .icon-back {
+          display: block;
+          padding: 9px;
+          font-size: $font-size-large-x;
+          color: $color-theme;
+          transform: rotate(-90deg);
+        }
+      }
+      .title {
+        width: 70%;
+        margin: 0 auto;
+        line-height: 40px;
+        text-align: center;
+        @include no-wrap();
+        font-size: $font-size-large;
+        color: $color-text;
+      }
+      .subtitle {
+        line-height: 20px;
+        text-align: center;
+        font-size: $font-size-medium;
+        color: $color-text;
+      }
+    }
+    .middle {
+      position: fixed;
+      width: 100%;
+      top: 80px;
+      bottom: 170px;
+      white-space: nowrap;
+      font-size: 0;
+      .middle-l {
+        display: inline-block;
+        vertical-align: top;
+        position: relative;
+        width: 100%;
+        height: 0;
+        padding-top: 80%;
+        .cd-wrapper {
+          position: absolute;
+          left: 10%;
+          top: 0;
+          width: 80%;
+          height: 100%;
+          .cd {
+            width: 100%;
+            height: 100%;
+            box-sizing: border-box;
+            border: 10px solid rgba(255, 255, 255, 0.1);
+            border-radius: 50%;
+            &.play {
+              animation: rotate 20s linear infinite;
+            }
+            &.pause {
+              animation-play-state: paused;
+            }
+            .image {
+              position: absolute;
+              left: 0;
+              top: 0;
+              width: 100%;
+              height: 100%;
+              border-radius: 50%;
+            }
+          }
+        }
+        .playing-lyric-wrapper {
+          width: 80%;
+          margin: 30px auto 0 auto;
+          overflow: hidden;
+          text-align: center;
+          .playing-lyric {
+            height: 20px;
+            line-height: 20px;
+            font-size: $font-size-medium;
+            color: $color-text-l;
+          }
+        }
+      }
+      .middle-r {
+        display: inline-block;
+        vertical-align: top;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        .lyric-wrapper {
+          width: 80%;
+          margin: 0 auto;
+          overflow: hidden;
+          text-align: center;
+          .text {
+            line-height: 32px;
+            color: $color-text-l;
+            font-size: $font-size-medium;
+            &.current {
+              color: $color-text;
+            }
+          }
+        }
+      }
+    }
+    .bottom {
+      position: absolute;
+      bottom: 50px;
+      width: 100%;
+      .dot-wrapper {
+        text-align: center;
+        font-size: 0;
+        .dot {
+          display: inline-block;
+          vertical-align: middle;
+          margin: 0 4px;
+          width: 8px;
+          height: 8px;
+          border-radius: 50%;
+          background: $color-text-l;
+          &.active {
+            width: 20px;
+            border-radius: 5px;
+            background: $color-text-ll;
+          }
+        }
+      }
+      .progress-wrapper {
+        display: flex;
+        align-items: center;
+        width: 80%;
+        margin: 0px auto;
+        padding: 10px 0;
+        .time {
+          color: $color-text;
+          font-size: $font-size-small;
+          flex: 0 0 30px;
+          line-height: 30px;
+          width: 30px;
+          &.time-l {
+            text-align: left;
+          }
+          &.time-r {
+            text-align: right;
+          }
+        }
+        .progress-bar-wrapper {
+          flex: 1;
+        }
+      }
+      .operators {
+        display: flex;
+        align-items: center;
+        .icon {
+          flex: 1;
+          color: $color-theme;
+          &.disable {
+            color: $color-theme-d;
+          }
+          i {
+            font-size: 30px;
+          }
+        }
+        .i-left {
+          text-align: right;
+        }
+        .i-center {
+          padding: 0 20px;
+          text-align: center;
+          i {
+            font-size: 40px;
+          }
+        }
+        .i-right {
+          text-align: left;
+        }
+        .icon-favorite {
+          color: $color-sub-theme;
+        }
+      }
+    }
+    &.normal-enter-active,
+    &.normal-leave-active {
+      transition: all 0.4s;
+      .top,
+      .bottom {
+        transition: all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32);
+      }
+    }
+    &.normal-enter,
+    &.normal-leave-to {
+      opacity: 0;
+      .top {
+        transform: translate3d(0, -100px, 0);
+      }
+      .bottom {
+        transform: translate3d(0, 100px, 0);
+      }
+    }
+  }
+  .mini-player {
+    display: flex;
+    align-items: center;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    z-index: 180;
+    width: 100%;
+    height: 60px;
+    background: $color-highlight-background;
+    &.mini-enter-active,
+    &.mini-leave-active {
+      transition: all 0.4s;
+    }
+    &.mini-enter,
+    &.mini-leave-to {
+      opacity: 0;
+    }
+    .icon {
+      flex: 0 0 40px;
+      width: 40px;
+      padding: 0 10px 0 20px;
+      img {
+        border-radius: 50%;
+        &.play {
+          animation: rotate 10s linear infinite;
+        }
+        &.pause {
+          animation-play-state: paused;
+        }
+      }
+    }
+    .text {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      flex: 1;
+      line-height: 20px;
+      overflow: hidden;
+      .name {
+        margin-bottom: 2px;
+        @include no-wrap();
+        font-size: $font-size-medium;
+        color: $color-text;
+      }
+      .desc {
+        @include no-wrap();
+        font-size: $font-size-small;
+        color: $color-text-d;
+      }
+    }
+    .control {
+      flex: 0 0 30px;
+      width: 30px;
+      padding: 0 10px;
+      .icon-play-mini,
+      .icon-pause-mini,
+      .icon-playlist {
+        font-size: 30px;
+        color: $color-theme-d;
+      }
+      .icon-mini {
+        font-size: 32px;
         position: absolute;
         left: 0;
         top: 0;
-        width: 100%;
-        height: 100%;
-        z-index: -1;
-        opacity: 0.6;
-        filter: blur(20px);
-      }
-      .top{
-        position: relative;
-        margin-bottom: 25px;
-        .back{
-          position:absolute;
-          top: 0;
-          left: 6px;
-          z-index: 50;
-          .icon-back{
-            display: block;
-            padding: 9px;
-            font-size: $font-size-large-x;
-            color: $color-theme;
-            transform: rotate(-90deg);
-          }
-        }
-        .title{
-          width: 70%;
-          margin: 0 auto;
-          line-height: 40px;
-          text-align: center;
-          @include no-wrap();
-          font-size: $font-size-large;
-          color: $color-text;
-        }
-        .subtitle{
-          line-height: 20px;
-          text-align: center;
-          font-size: $font-size-medium;
-          color: $color-text;
-        }
-      }
-      .middle{
-        position: fixed;
-        width: 100%;
-        top: 80px;
-        bottom: 170px;
-        white-space: nowrap;
-        font-size: 0;
-        .middle-l{
-          display: inline-block;
-          vertical-align: top;
-          position: relative;
-          width: 100%;
-          height: 0;
-          padding-top: 80%;
-          .cd-wrapper{
-            position: absolute;
-            left: 10%;
-            top: 0;
-            width: 80%;
-            height: 100%;
-            .cd{
-              width: 100%;
-              height: 100%;
-              box-sizing: border-box;
-              border: 10px solid rgba(255, 255, 255, 0.1);
-              border-radius: 50%;
-              &.play{
-                animation: rotate 20s linear infinite;
-              }
-              &.pause{
-                animation-play-state: paused;
-              }
-              .image{
-                position: absolute;
-                left: 0;
-                top: 0;
-                width: 100%;
-                height: 100%;
-                border-radius: 50%;
-              }
-            }
-          }
-          .playing-lyric-wrapper{
-            width: 80%;
-            margin: 30px auto 0 auto;
-            overflow: hidden;
-            text-align: center;
-            .playing-lyric{
-              height: 20px;
-              line-height: 20px;
-              font-size: $font-size-medium;
-              color: $color-text-l;
-            }
-          }
-        }
-        .middle-r{
-          display: inline-block;
-          vertical-align: top;
-          width: 100%;
-          height: 100%;
-          overflow: hidden;
-          .lyric-wrapper{
-            width: 80%;
-            margin: 0 auto;
-            overflow: hidden;
-            text-align: center;
-            .text{
-              line-height: 32px;
-              color: $color-text-l;
-              font-size: $font-size-medium;
-              &.current{
-                color: $color-text;
-              }
-            }
-          }
-        }
-      }
-      .bottom{
-        position: absolute;
-        bottom: 50px;
-        width: 100%;
-        .dot-wrapper{
-          text-align: center;
-          font-size: 0;
-          .dot{
-            display: inline-block;
-            vertical-align: middle;
-            margin: 0 4px;
-            width: 8px;
-            height: 8px;
-            border-radius: 50%;
-            background: $color-text-l;
-            &.active{
-              width: 20px;
-              border-radius: 5px;
-              background: $color-text-ll;
-            }
-          }
-        }
-        .progress-wrapper{
-          display: flex;
-          align-items: center;
-          width: 80%;
-          margin: 0px auto;
-          padding: 10px 0;
-          .time{
-            color: $color-text;
-            font-size: $font-size-small;
-            flex: 0 0 30px;
-            line-height: 30px;
-            width: 30px;
-            &.time-l{
-              text-align: left;
-            }
-            &.time-r{
-              text-align: right;
-            }
-          }
-          .progress-bar-wrapper{
-            flex: 1;
-          }
-        }
-        .operators{
-          display: flex;
-          align-items: center;
-          .icon{
-            flex: 1;
-            color: $color-theme;
-            &.disable{
-              color: $color-theme-d;
-            }
-            i{
-              font-size: 30px;
-            }
-          }
-          .i-left{
-            text-align: right
-          }
-          .i-center{
-            padding: 0 20px;
-            text-align: center;
-            i{
-              font-size: 40px;
-            }
-          }
-          .i-right{
-            text-align: left;
-          }
-          .icon-favorite{
-            color: $color-sub-theme
-          }
-        }
-      }
-      &.normal-enter-active, &.normal-leave-active{
-        transition: all 0.4s;
-        .top, .bottom{
-          transition: all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32)
-        }
-      }
-      &.normal-enter, &.normal-leave-to{
-        opacity: 0;
-        .top{
-          transform: translate3d(0, -100px, 0)
-        }
-        .bottom{    
-          transform: translate3d(0, 100px, 0)
-        }
       }
     }
-    .mini-player{
-      display: flex;
-      align-items: center;
-      position: fixed;
-      left: 0;
-      bottom: 0;
-      z-index: 180;
-      width: 100%;
-      height: 60px;
-      background: $color-highlight-background;
-      &.mini-enter-active, &.mini-leave-active{
-        transition: all 0.4s;
-      }
-      &.mini-enter, &.mini-leave-to{
-        opacity: 0;
-      }
-      .icon{
-        flex: 0 0 40px;
-        width: 40px;
-        padding: 0 10px 0 20px;
-        img{
-          border-radius: 50%;
-          &.play{
-            animation: rotate 10s linear infinite
-          }
-          &.pause{
-            animation-play-state: paused
-          }
-        }
-      }
-      .text{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        flex: 1;
-        line-height: 20px;
-        overflow: hidden;
-        .name{
-          margin-bottom: 2px;
-          @include no-wrap();
-          font-size: $font-size-medium;
-          color: $color-text;
-        }
-        .desc{
-          @include no-wrap();
-          font-size: $font-size-small;
-          color: $color-text-d;
-        }
-      }
-      .control{
-        flex: 0 0 30px;
-        width: 30px;
-        padding: 0 10px;
-        .icon-play-mini, .icon-pause-mini, .icon-playlist{
-          font-size: 30px;
-          color: $color-theme-d;
-        }
-        .icon-mini{
-          font-size: 32px;
-          position: absolute;
-          left: 0;
-          top: 0;
-        }
-      }
-    }
-  @keyframes rotate{
-    0%{
+  }
+  @keyframes rotate {
+    0% {
       transform: rotate(0);
     }
-    100%{
+    100% {
       transform: rotate(360deg);
     }
   }
