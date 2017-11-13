878f125083786273cf6127dfc42eb4d969e092bd
Gavin <876575488@qq.com>
2017-11-13 22:52:56 +0800

    完善数据部分

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,93 +1,115 @@
 <template>
-  <div class="rank">
-      <div class="toplist">
+  <div class="rank" ref="rank">
+      <scroll :data="topList" class="toplist" ref="topList">
           <ul>
-              <li class="item">
+              <li class="item" v-for="item in topList">
                   <div class="icon">
-                      <img width="100" height="100" />
+                      <img width="100" height="100" v-lazy="item.picUrl"/>
                   </div>
                   <ul class="songlist">
-                      <li class="song">
-                          <span></span>
-                          <span></span>
+                      <li class="song" v-for="(song, index) in item.songList">
+                          <span>{{ index+ 1}}</span>
+                          <span>{{song.songname}}-{{song.singername}}</span>
                       </li>
                   </ul>
               </li>
           </ul>
-      </div>
+          <div class="loading-container" v-show="!topList.length">
+              <loading></loading>
+          </div>
+      </scroll>
       <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import {getTopList} from 'api/rank'
-import {ERR_OK} from 'api/config'
+import { getTopList } from 'api/rank'
+import { ERR_OK } from 'api/config'
+import Scroll from 'base/scroll/scroll'
+import Loading from 'base/loading/loading'
+import { playlistMixin } from 'common/js/mixin'
 
-    export default {
-        created() {
-            this._getTopList()
-        },
-        methods: {
-            _getTopList() {
-                getTopList().then((res) => {
-                    if (res.code === ERR_OK) {
-                        console.log(res)
-                    }
-                })
-            }
+export default {
+    mixins: [playlistMixin],
+    created() {
+        this._getTopList()
+    },
+    data() {
+        return {
+            topList: []
         }
+    },
+    methods: {
+        handdlePlaylist(playlist) {
+            const bottom = playlist.length ? '60px' : ''
+            this.$refs.rank.style.bottom = bottom
+            this.$refs.topList.refresh()
+        },
+        _getTopList() {
+            getTopList().then((res) => {
+                if (res.code === ERR_OK) {
+                    this.topList = res.data.topList
+                }
+            })
+        }
+    },
+    components: {
+        Scroll,
+        Loading
     }
+}
 </script>
 
 <style scoped lang="scss">
 @import "~common/stylus/variable";
 @import "~common/stylus/mixin";
 
-.rank{
-    position: fixed;
-    width: 100%;
-    top: 88px;
-    bottom: 0;
-    .toplist{
-        height: 100%;
+.rank {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+  .toplist {
+    height: 100%;
+    overflow: hidden;
+    .item {
+      display: flex;
+      margin: 0 20px;
+      padding-top: 20px;
+      height: 100px;
+      &:last-child {
+        padding-bottom: 20px;
+      }
+      .icon {
+        flex: 0 0 100px;
+        width: 100px;
+        height: 100px;
+      }
+      .songlist {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 0 20px;
+        height: 100px;
         overflow: hidden;
-        .item{
-            display: flex;
-            margin: 0 20px;
-            padding-top: 20px;
-            height: 100px;
-            &:last-child{
-                padding-bottom: 20px;
-            }
-            .icon{
-                flex: 0 0 100px;
-                width: 100px;
-                height: 100px;
-            }
-            .songlist{
-                flex: 1;
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                padding: 0 20px;
-                height: 100px;
-                overflow: hidden;
-                background: $color-highlight-background;
-                color: $color-text-d;
-                font-size: $font-size-small;
-                .song{
-                    @include no-wrap();
-                    line-height: 26px;
-                }
-            }
+        background: #62997A;
+        color: rgba(255, 255, 255, 0.8);
+        font-size: $font-size-small;
+        .song {
+          @include no-wrap();
+          line-height: 26px;
         }
-        .loading-container{
-            position: absolute;
-            width: 100%;
-            top: 50%;
-            transform: translate(-50%)
-        }
+      }
     }
+    .loading-container {
+      position: absolute;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
 }
 </style>
 
