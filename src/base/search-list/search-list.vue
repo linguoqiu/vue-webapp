5dfe5520a9c2f5db919fede88c565dc9be745aec
Gavin <876575488@qq.com>
2017-12-09 23:51:16 +0800

    add search-list

---
+++ src/base/search-list/search-list.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="search-list" v-show="searches.length">
+      <ul>
+          <li class="search-item" v-for="item in searches">
+              <span class="text">{{item}}</span>
+              <span class="icon">
+                  <i class="icon-delete"></i>
+              </span>
+          </li>
+      </ul>
+  </div>
+</template>
+
+
+<script type="text/ecmascript-6">
+export default {
+  props: {
+      searches: {
+          type: Array,
+          default: []
+      }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~common/stylus/variable";
+
+.search-list{
+    .search-item{
+        display: flex;
+        align-items: center;
+        height: 40px;
+        .text{
+            flex: 1;
+            color: $color-text-l
+        }
+        .icon{
+            @include extend-click();
+            icon-delete{
+                font-size: $font-size-small;
+                color: $color-text-d;
+            }
+        }
+    }
+}
+</style>
+
